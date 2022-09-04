27253416c83f6ea1eae70258a32340f98d9ffb6b
nasty <anastaishastil2002@gmail.com>
2022-09-04 17:35:57 +0300

    Updated

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character < 'a' || character > 'z') {
+            System.out.println("wrong alphabet!");
+        } else if (character == 'a' || character == 'e' || character == 'y' || character == 'u' || character == 'i' || character == 'o') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
