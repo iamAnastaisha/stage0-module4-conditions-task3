9b4cba6930332753a841d52cce2676affc4172a8
nasty <anastaishastil2002@gmail.com>
2022-09-04 17:42:36 +0300

    Updated

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,12 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
-        if (character < 'a' || character > 'z') {
-            System.out.println("wrong alphabet!");
-        } else if (character == 'a' || character == 'e' || character == 'y' || character == 'u' || character == 'i' || character == 'o') {
+        if (character == 'a' || character == 'e' || character == 'y' || character == 'u' || character == 'i'
+                || character == 'o' || character == 'A' || character == 'E' || character == 'Y' || character == 'U' || character == 'I' || character == 'O') {
             System.out.println("Vowel");
-        } else {
+        } else if (character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z') {
             System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task3;
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
         if (month == 2) {
-            System.out.println(29);
+            System.out.println(28);
         } else if (month == 4 || month == 6 || month == 9 || month == 11) {
             System.out.println(30);
         } else if (month < 1 || month > 12) {
